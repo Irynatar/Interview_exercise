a3f0aa297b0d8377b57a468507433131945410da
Iryna <iryna.tar18@gmail.com>
2021-03-24 16:20:05 -0600

    change reverse function into reverse1 function

--- reverse_string.js
+++ reverse_string.js
@@ -10,11 +10,11 @@ console.log(reverse("Iryna!"))
 
 //second method
 
-const reverse1 = function reverse(str) {
+const reverse1 = function (str) {
     let newStr = ''
     for (let i=str.length-1; i>=0; i--) {
       newStr = newStr + str[i]
   }
    return newStr
 }
-console.log(reverse("Iryna!"))
+console.log(reverse1("Iryna!"))
