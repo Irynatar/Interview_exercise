8d3244830d3029c3bd6283bbc52feee86e198450
Iryna <iryna.tar18@gmail.com>
2021-03-24 10:46:34 -0600

    added second reverse function

--- reverse_string.js
+++ reverse_string.js
@@ -1,6 +1,20 @@
+
+//first method
+
 const reverse = function (str) {
     let newStr = str.split("").reverse().join("");
     return newStr
 }
 
+console.log(reverse("Iryna!"))
+
+//second method
+
+const reverse1 = function reverse(str) {
+    let newStr = ''
+    for (let i=str.length-1; i>=0; i--) {
+      newStr = newStr + str[i]
+  }
+   return newStr
+}
 console.log(reverse("Iryna!"))
